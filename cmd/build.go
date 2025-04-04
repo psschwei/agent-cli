75b1506fd62943e8d3b41d79713698d56d40c4c1
Paul S. Schweigert <paul@paulschweigert.com>
2025-04-04 12:21:15 -0400

    update build command to use docker
    Signed-off-by: Paul S. Schweigert <paul@paulschweigert.com>

--- cmd/build.go
+++ cmd/build.go
@@ -4,16 +4,17 @@ Copyright © 2025 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
-	"bufio"
-	"embed"
+	_ "embed"
 	"fmt"
 	"os"
-	"strings"
+	"os/exec"
 
 	"github.com/spf13/cobra"
 )
 
-var sourceFile string
+var agentImage string
+var workingDirectory string
+var imageTag string
 
 // buildCmd represents the build command
 var buildCmd = &cobra.Command{
@@ -22,9 +23,9 @@ var buildCmd = &cobra.Command{
 	Long: `Build your agent
 
 Example:
-    hive-cli build -f source.py`,
+    hive-cli build -d "/home/user/my-agent/src" -t "my-agent:latest"`,
 	RunE: func(cmd *cobra.Command, args []string) error {
-		return buildAgent(sourceFile)
+		return buildAgent(workingDirectory, imageTag)
 	},
 }
 
@@ -35,79 +36,71 @@ func init() {
 
 	// Cobra supports Persistent Flags which will work for this command
 	// and all subcommands, e.g.:
-	buildCmd.PersistentFlags().StringVarP(&sourceFile, "file", "f", "", "agent source code file")
+	buildCmd.PersistentFlags().StringVarP(&workingDirectory, "dir", "d", "", "directory with agent code")
+	buildCmd.PersistentFlags().StringVarP(&imageTag, "tag", "t", "", "image tag")
 
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
 	// buildCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
 
-//go:embed template.py
-var f embed.FS
+//go:embed Dockerfile
+var d []byte
 
-func buildAgent(source string) error {
+func buildAgent(dir, tag string) error {
 
-	// convert source to string with proper indentation
-	// write template to new file, replacing ## INSERT ## line with user code
-
-	// Open the execution template
-	template := "template.py"
-	templateFile, err := f.Open(template)
-	// templateFile, err := os.Open(template)
-	if err != nil {
-		fmt.Println("Error opening file:", err)
-		return err
-	}
-	defer templateFile.Close()
-
-	// Open user source code
-	sourceFile, err := os.Open(source)
-	if err != nil {
-		fmt.Println("Error opening file:", err)
-		return err
-	}
-	defer templateFile.Close()
-
-	// Create a new file to write the modified lines
-	agentFile, err := os.Create("agent.py")
-	if err != nil {
-		fmt.Println("Error creating modified file:", err)
-		return err
-	}
-	defer agentFile.Close()
-
-	// Create a scanner to read the existing file line by line
-	scanner := bufio.NewScanner(templateFile)
-
-	// Write the lines of the existing file to the modified file
-	for scanner.Scan() {
-		line := scanner.Text()
-
-		// Check if the line is the target line
-		if strings.Contains(line, "##### INSERT USER CODE HERE #####") {
-			// Add the new file to the target line
-			codeScanner := bufio.NewScanner(sourceFile)
-			for codeScanner.Scan() {
-				codeLine := codeScanner.Text()
-				_, err := agentFile.WriteString("    " + codeLine + "\n")
-				if err != nil {
-					fmt.Println("Error writing to modified file:", err)
-					return err
-				}
-			}
-		} else {
-			// Write the existing line to the modified file
-			_, err := agentFile.WriteString(line + "\n")
-			if err != nil {
-				fmt.Println("Error writing to modified file:", err)
-				return err
-			}
-		}
+	// 1. Validate that docker is on system
+	if err := checkDocker(); err != nil {
+		return fmt.Errorf("%w", err)
 	}
 
-	fmt.Println("Runnable AI Agent created: agent.py.")
-	fmt.Println("To run locally, use this command:")
-	fmt.Println("hive-cli run -f agent.py")
+	// 2. Validate directory exists (or use PWD if no value)
+	if dir == "" {
+		dir, _ = os.Getwd()
+	}
+	if _, err := os.Stat(dir); err != nil {
+		return fmt.Errorf("%w", err)
+	}
 
+	// 3. Validate requirements.txt and run_agent.py files exist in dir
+	if _, err := os.Stat(dir + "/requirements.txt"); err != nil {
+		return fmt.Errorf("Missing file: requirements.txt required")
+	}
+	if _, err := os.Stat(dir + "/run_agent.py"); err != nil {
+		return fmt.Errorf("Missing file: run_agent.py required")
+	}
+
+	// 4. Exec docker build command
+	// create tempdir for Dockerfile
+	dockerFile, _ := os.CreateTemp("", "Dockerfile")
+	defer dockerFile.Close()
+	if _, err := dockerFile.Write(d); err != nil {
+		return fmt.Errorf("%w", err)
+	}
+
+	buildContainer := exec.Command("docker", "build", "-t", tag, "-f", dockerFile.Name(), dir)
+	if err := runCommandWithOutput(buildContainer); err != nil {
+		return fmt.Errorf("Error building agent: %w", err)
+	}
+
+	return nil
+}
+
+// checkDocker checks that Docker is running on the users local system.
+func checkDocker() error {
+	dockerCheck := exec.Command("docker", "stats", "--no-stream")
+	if err := dockerCheck.Run(); err != nil {
+		return fmt.Errorf("docker not running")
+	}
+	return nil
+}
+
+func runCommandWithOutput(c *exec.Cmd) error {
+	c.Stdout = os.Stdout
+	c.Stderr = os.Stderr
+	if err := c.Run(); err != nil {
+		return fmt.Errorf("piping output: %w", err)
+	}
+	fmt.Print("\n")
 	return nil
 }
