86afc68842d13b17641d596bf572a743a437ee21
Paul Schweigert <paul@paulschweigert.com>
2025-04-04 14:56:28 -0400

    add deploy to kubernetes functionality (#2)
    Signed-off-by: Paul S. Schweigert <paul@paulschweigert.com>

--- cmd/build.go
+++ cmd/build.go
@@ -1,6 +1,3 @@
-/*
-Copyright © 2025 NAME HERE <EMAIL ADDRESS>
-*/
 package cmd
 
 import (
@@ -9,6 +6,7 @@ import (
 	"os"
 	"os/exec"
 
+	"github.com/psschwei/hive-cli/pkg/utils"
 	"github.com/spf13/cobra"
 )
 
@@ -20,7 +18,7 @@ var imageTag string
 var buildCmd = &cobra.Command{
 	Use:   "build",
 	Short: "Build your agent",
-	Long: `Build your agent
+	Long: `Build your agent into an OCI container.
 
 Example:
     hive-cli build -d "/home/user/my-agent/src" -t "my-agent:latest"`,
@@ -36,7 +34,7 @@ func init() {
 
 	// Cobra supports Persistent Flags which will work for this command
 	// and all subcommands, e.g.:
-	buildCmd.PersistentFlags().StringVarP(&workingDirectory, "dir", "d", "", "directory with agent code")
+	buildCmd.PersistentFlags().StringVarP(&workingDirectory, "dir", "d", "", "directory with agent code (default: $PWD)")
 	buildCmd.PersistentFlags().StringVarP(&imageTag, "tag", "t", "", "image tag")
 
 	// Cobra supports local flags which will only run when this command
@@ -50,7 +48,7 @@ var d []byte
 func buildAgent(dir, tag string) error {
 
 	// 1. Validate that docker is on system
-	if err := checkDocker(); err != nil {
+	if err := utils.CheckDocker(); err != nil {
 		return fmt.Errorf("%w", err)
 	}
 
@@ -71,7 +69,6 @@ func buildAgent(dir, tag string) error {
 	}
 
 	// 4. Exec docker build command
-	// create tempdir for Dockerfile
 	dockerFile, _ := os.CreateTemp("", "Dockerfile")
 	defer dockerFile.Close()
 	if _, err := dockerFile.Write(d); err != nil {
@@ -79,28 +76,9 @@ func buildAgent(dir, tag string) error {
 	}
 
 	buildContainer := exec.Command("docker", "build", "-t", tag, "-f", dockerFile.Name(), dir)
-	if err := runCommandWithOutput(buildContainer); err != nil {
+	if err := utils.RunCommandWithOutput(buildContainer); err != nil {
 		return fmt.Errorf("Error building agent: %w", err)
 	}
 
-	return nil
-}
-
-// checkDocker checks that Docker is running on the users local system.
-func checkDocker() error {
-	dockerCheck := exec.Command("docker", "stats", "--no-stream")
-	if err := dockerCheck.Run(); err != nil {
-		return fmt.Errorf("docker not running")
-	}
-	return nil
-}
-
-func runCommandWithOutput(c *exec.Cmd) error {
-	c.Stdout = os.Stdout
-	c.Stderr = os.Stderr
-	if err := c.Run(); err != nil {
-		return fmt.Errorf("piping output: %w", err)
-	}
-	fmt.Print("\n")
 	return nil
 }
