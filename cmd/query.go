feec94d4c8767738bc5551b443eaf573c6f959aa
Paul Schweigert <paul@paulschweigert.com>
2025-04-04 15:49:34 -0400

    add query function (#3)
    Signed-off-by: Paul S. Schweigert <paul@paulschweigert.com>

---
+++ cmd/query.go
@@ -0,0 +1,68 @@
+package cmd
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"time"
+
+	"github.com/spf13/cobra"
+)
+
+var queryPrompt string
+var queryHost string
+
+type acp struct {
+	Prompt   string  `json:"prompt"`
+	Response *string `json:"response"`
+}
+
+// queryCmd represents the query command
+var queryCmd = &cobra.Command{
+	Use:   "query",
+	Short: "Send a query to an agent",
+	Long: `Send a query to an agent.
+
+Example:
+   hive-cli query -p "What is the weather in San Francisco?" -h https://my-agent.example.com`,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		return queryAgent(queryPrompt, queryHost)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(queryCmd)
+	queryCmd.PersistentFlags().StringVarP(&queryPrompt, "prompt", "p", "", "Prompt to send to the agent")
+	queryCmd.PersistentFlags().StringVarP(&queryHost, "host", "u", "http://localhost:8080", "Host to query agent (default: http://localhost:8080)")
+}
+
+func queryAgent(query, url string) error {
+	fmt.Printf("Prompt: %s\n", query)
+	payload := map[string]string{"prompt": query}
+	payloadBytes, _ := json.Marshal(payload)
+	response := acp{}
+
+	req, err := http.NewRequest(http.MethodPost, url, bytes.NewBuffer(payloadBytes))
+	req.Header.Add("Content-Type", "application/json")
+
+	client := http.Client{
+		Timeout: 300 * time.Second,
+	}
+
+	res, err := client.Do(req)
+	if err != nil {
+		return fmt.Errorf("unable to query agent: %w", err)
+	}
+
+	resBody, _ := io.ReadAll(res.Body)
+
+	if err = json.Unmarshal(resBody, &response); err != nil {
+		return fmt.Errorf("unable to process agent response: %w", err)
+	}
+
+	fmt.Printf("Response: %s\n", *response.Response)
+
+	return nil
+}
